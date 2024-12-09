156c587bd37c2406eeb720811a09b4682705c610
white-blue-moon <eoiuco@gmail.com>
2024-12-09 15:39:09 +0900

    배너 이미지 리스트 관리 테이블 쿼리 추가

---
+++ src/db/createTable.sql
@@ -0,0 +1,11 @@
+-- 배너 이미지 리스트 관리 테이블 TODO (img_url 의 경우 서버에서 공백 포함 여부 체크하기)
+DROP TABLE IF EXISTS ref_bannerInfo;
+CREATE TABLE ref_bannerInfo (
+    id          INT AUTO_INCREMENT PRIMARY KEY COMMENT '기본 키 컬럼',
+    version     INT             NOT NULL COMMENT '배너 버전',
+    banner_type TINYINT         NOT NULL COMMENT '배너 타입(1: 메인 포커스 배너, 2: 뉴스 배너 big, 3: 뉴스 배너 small)',
+    img_url     VARCHAR(255)    NOT NULL COMMENT '이미지 링크',
+    img_order   SMALLINT        NOT NULL COMMENT '이미지 출력 순서',
+    img_comment VARCHAR(255)    COMMENT '이미지 설명',  -- 기본값 없음 (NULL 허용)
+    CONSTRAINT unique_ref_bannerInfo UNIQUE (version, banner_type, img_order)
+) CHARSET=utf8 COMMENT='배너 이미지 리스트 관리 테이블';
---
+++ src/db/insertData.sql
@@ -0,0 +1,13 @@
+-- 명시적으로 .sql 파일 문자셋 설정 (한글 깨짐 방지)
+SET NAMES 'utf8';
+
+-- 배너 리스트 관리 테이블 1 버전 정보 추가
+DELETE FROM ref_bannerInfo WHERE version = 1;
+INSERT INTO ref_bannerInfo (version, banner_type, img_url, img_order, img_comment) VALUES(1, 1, 'https://bbscdn.df.nexon.com/data6/commu/202411/0a3de54d-8ab7-0919-3ef8-266c04a409e4.jpg', 1, '빙결사');
+INSERT INTO ref_bannerInfo (version, banner_type, img_url, img_order, img_comment) VALUES(1, 1, 'https://bbscdn.df.nexon.com/data6/commu/202412/23e9140b-102e-f4c2-9bfb-f48f7dc1813a.jpg', 2, '페이트 콜라보');
+INSERT INTO ref_bannerInfo (version, banner_type, img_url, img_order, img_comment) VALUES(1, 1, 'https://bbscdn.df.nexon.com/data6/commu/202412/bebf85e9-c22d-e4b1-938e-3e862ebb1ad3.jpg', 3, '아트 공모전');
+INSERT INTO ref_bannerInfo (version, banner_type, img_url, img_order, img_comment) VALUES(1, 1, 'https://bbscdn.df.nexon.com/data6/commu/202411/ce8502c3-776d-0d74-3f57-0b919abcf14b.jpg', 4, '중천');
+INSERT INTO ref_bannerInfo (version, banner_type, img_url, img_order, img_comment) VALUES(1, 1, 'https://bbscdn.df.nexon.com/data6/commu/202411/8d466130-c079-f15b-2c1a-2233b298cfde.jpg', 5, '던파 ON 리뉴얼');
+INSERT INTO ref_bannerInfo (version, banner_type, img_url, img_order, img_comment) VALUES(1, 1, 'https://bbscdn.df.nexon.com/data6/commu/202411/05243a37-826d-0ef2-e2b5-9b6d8fe4de1a.jpg', 6, '겨울 방학 생활');
+INSERT INTO ref_bannerInfo (version, banner_type, img_url, img_order, img_comment) VALUES(1, 1, 'https://bbscdn.df.nexon.com/data6/commu/202411/cc55693a-3e3c-3310-c5fb-d160747652cd.jpg', 7, '스노우 메이지 추억 앨범');
+INSERT INTO ref_bannerInfo (version, banner_type, img_url, img_order, img_comment) VALUES(1, 1, 'https://bbscdn.df.nexon.com/data6/commu/202411/13a64cd4-684e-77f8-4799-f8fcd2330326.jpg', 8, '스노우 메이지 스펙업');
