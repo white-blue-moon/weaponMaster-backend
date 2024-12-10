b903b6c57583020f9d80fbcaf237eb93b192af32
white-blue-moon <eoiuco@gmail.com>
2024-12-10 17:40:55 +0900

    CORS 허용되도록 수정

---
+++ src/main/java/com/example/weaponMaster/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.weaponMaster.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 경로에 대해 CORS 설정
+                .allowedOrigins("http://localhost:8080")  // 허용할 클라이언트 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
+                .allowedHeaders("*")  // 허용할 헤더
+                .allowCredentials(true);  // 자격 증명 허용
+    }
+}
