c2b8840316a977cc658e2129e6931bdb4cc47edc
white-blue-moon <eoiuco@gmail.com>
2025-04-26 15:29:55 +0900

    favicon.ico 요청 에러는 무시하도록 수정

--- src/main/java/com/example/weaponMaster/modules/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/weaponMaster/modules/common/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.example.weaponMaster.modules.slack.constant.AdminSlackChannelType;
 import com.example.weaponMaster.modules.slack.constant.UserSlackNoticeType;
 import com.example.weaponMaster.modules.slack.entity.AdminSlackNotice;
 import com.example.weaponMaster.modules.slack.service.SlackService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,9 +21,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ApiResponse<String>> handleIllegalArgumentException(IllegalArgumentException e) {
         System.err.println(e.getMessage());
-
         slackService.sendMessageAdmin(AdminSlackChannelType.BACK_END_ERROR_NOTICE, String.format(
-                "`[400 Bad Request Error]`\n" +
+                "`400 Bad Request Error`\n" +
                         "```\n" +
                         " %s\n" +
                         "```",
@@ -34,11 +34,17 @@ public class GlobalExceptionHandler {
 
     // 예상하지 못한 오류 (500 Internal Server Error)
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<String>> handleGeneralException(Exception e) {
-        System.err.println(e.getMessage());
+    public ResponseEntity<ApiResponse<String>> handleGeneralException(HttpServletRequest request, Exception e) {
+        String requestURI = request.getRequestURI();
 
+        // favicon.ico 요청은 무시
+        if ("/favicon.ico".equals(requestURI)) {
+            return ResponseEntity.ok().build(); // 아무것도 하지 않고 OK 반환
+        }
+
+        System.err.println(e.getMessage());
         slackService.sendMessageAdmin(AdminSlackChannelType.BACK_END_ERROR_NOTICE, String.format(
-                "`[500 Internal Server Error]`\n" +
+                "`500 Internal Server Error`\n" +
                         "```\n" +
                         "%s\n" +
                         "```",
