e2a4a4c2e957b14a9e642689f949562b038ceb04
white-blue-moon <eoiuco@gmail.com>
2025-04-28 12:29:56 +0900

    에러 발생 시 스택 트레이스도 함께 남기도록 수정

--- src/main/java/com/example/weaponMaster/modules/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/weaponMaster/modules/common/exception/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import com.example.weaponMaster.modules.slack.entity.AdminSlackNotice;
 import com.example.weaponMaster.modules.slack.service.SlackService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,12 +17,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
-    private final SlackService slackService;
+    // logger 로 에러를 남기면 스택 트레이스를 로깅할 수 있음
+    private static final Logger       logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private final        SlackService slackService;
 
     // 클라이언트가 잘못된 요청을 보냈을 때 (400 Bad Request)
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ApiResponse<String>> handleIllegalArgumentException(IllegalArgumentException e) {
-        System.err.println(e.getMessage());
+        logger.error("Bad Request error occurred: ", e);
         slackService.sendMessageAdmin(AdminSlackChannelType.BACK_END_ERROR_NOTICE, String.format(
                 "`400 Bad Request Error`\n" +
                         "```\n" +
@@ -42,7 +46,7 @@ public class GlobalExceptionHandler {
             return ResponseEntity.ok().build(); // 아무것도 하지 않고 OK 반환
         }
 
-        System.err.println(e.getMessage());
+        logger.error("Unexpected error occurred: ", e);
         slackService.sendMessageAdmin(AdminSlackChannelType.BACK_END_ERROR_NOTICE, String.format(
                 "`500 Internal Server Error`\n" +
                         "```\n" +
