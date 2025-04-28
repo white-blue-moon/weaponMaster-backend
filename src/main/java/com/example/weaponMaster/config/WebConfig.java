7d18bd43c044a7de9fe35cc527fa6ce6caaefce4
white-blue-moon <eoiuco@gmail.com>
2025-04-28 14:58:30 +0900

    PATCH 통신 방식 허용하도록 수정

--- src/main/java/com/example/weaponMaster/config/WebConfig.java
+++ src/main/java/com/example/weaponMaster/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("http://localhost:8080")  // 허용할 클라이언트 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
+                .allowedOrigins("http://localhost:8080")  // 허용할 클라이언트 도메인 -> TODO config 등으로 경로 받아오도록 수정 필요
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 허용할 헤더
                 .allowCredentials(true);  // 자격 증명 허용
     }
