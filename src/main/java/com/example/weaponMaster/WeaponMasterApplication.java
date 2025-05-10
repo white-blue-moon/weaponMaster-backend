0dc210c3b0b3da3107056c338e63787e4e1b632e
white-blue-moon <eoiuco@gmail.com>
2025-05-10 17:39:05 +0900

    .env 참조 방식 -> config 내 .yml 참조하도록 변경

--- src/main/java/com/example/weaponMaster/WeaponMasterApplication.java
+++ src/main/java/com/example/weaponMaster/WeaponMasterApplication.java
@@ -1,6 +1,5 @@
 package com.example.weaponMaster;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,14 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class WeaponMasterApplication {
 
-	private static final Dotenv dotenv = Dotenv.load();
-
 	public static void main(String[] args) {
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("NEOPLE_API_KEY", dotenv.get("NEOPLE_API_KEY"));
-
 		SpringApplication.run(WeaponMasterApplication.class, args);
 	}
 }
