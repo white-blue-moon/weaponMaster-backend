8f4e6b2960f8e3421d30582c9f1115f5ed165b6a
white-blue-moon <eoiuco@gmail.com>
2025-05-11 14:33:48 +0900

    WebConfig - 허용 클라이언트 도메인 config 로 받도록 수정

--- src/main/java/com/example/weaponMaster/config/WebConfig.java
+++ src/main/java/com/example/weaponMaster/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.weaponMaster.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("http://localhost:8080")  // 허용할 클라이언트 도메인 -> TODO config 등으로 경로 받아오도록 수정 필요
+                .allowedOrigins(allowedOrigins)  // 허용할 클라이언트 도메인
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 허용할 헤더
                 .allowCredentials(true);  // 자격 증명 허용
