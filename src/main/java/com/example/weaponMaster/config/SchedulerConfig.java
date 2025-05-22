91d0b638d36a08439cee8727c31576976898c22c
white-blue-moon <eoiuco@gmail.com>
2025-05-22 18:42:53 +0900

    taskScheduler -> 서버 종료될 때 함께 종료되도록 shutdown 추가

--- src/main/java/com/example/weaponMaster/config/SchedulerConfig.java
+++ src/main/java/com/example/weaponMaster/config/SchedulerConfig.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 @Configuration
 public class SchedulerConfig {
 
-    @Bean
+    @Bean(destroyMethod = "shutdown")
     public ThreadPoolTaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(10); // 스레드 수(corePoolSize)와 실행할 수 있는 작업 수는 1:1 관계가 아님
