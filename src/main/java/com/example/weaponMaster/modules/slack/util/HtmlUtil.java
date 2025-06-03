a867f47fee44ae911684a7856e8bdc8de2666437
white-blue-moon <eoiuco@gmail.com>
2025-06-03 11:07:01 +0900

    슬랙 알림 메시지 - getPlainText() 함수 <div> 태그 공백 적용

--- src/main/java/com/example/weaponMaster/modules/slack/util/HtmlUtil.java
+++ src/main/java/com/example/weaponMaster/modules/slack/util/HtmlUtil.java
@@ -6,6 +6,7 @@ public class HtmlUtil {
         String plainText = contents
                 .replaceAll("(?i)<br\\s*/?>", "\n")             // <br>, <br/> → 줄바꿈
                 .replaceAll("(?i)</p>", "\n")                   // </p> → 줄바꿈
+                .replaceAll("(?i)<div[^>]*>", " ")              // 여는 <div> → 공백
                 .replaceAll("(?i)</h[1-6]>", "\n")              // 제목 태그 종료 → 줄바꿈
                 .replaceAll("(?i)</(em|strong|span)>", " ")     // 강조, span 태그 종료 → 공백으로 대체
                 .replaceAll("(?i)<[^>]*>", "")                  // 나머지 HTML 태그 제거
