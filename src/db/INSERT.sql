b29b9e62ac785c4da50c2a8e50e0f132dcbcebc5
white-blue-moon <eoiuco@gmail.com>
2025-06-17 17:17:18 +0900

    홈 > 캐릭터 배너 테스트 데이터 추가

--- src/db/INSERT.sql
+++ src/db/INSERT.sql
@@ -143,21 +143,118 @@ INSERT INTO ref_focus_banner_info (version, banner_type, img_url, img_order, img
 
 -- 홈 > 캐릭터 배너 (버전 1)
 DELETE FROM ref_character_banner WHERE version = 1;
-INSERT INTO ref_character_banner (version, character_type, character_desc, name_img_url, thumb_img_url, banner_order) VALUES (1, 4, '격투가(여)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt04.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_04.png', 1);
-INSERT INTO ref_character_banner (version, character_type, character_desc, name_img_url, thumb_img_url, banner_order) VALUES (1, 1, '귀검사(남)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt01.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_01.png', 2);
+INSERT INTO ref_character_banner (version, character_type, character_desc, name_img_url, thumb_img_url, banner_order) VALUES
+(1, 4, '격투가(여)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt04.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_04.png', 1),
+(1, 3, '격투가(남)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt03.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_03.png', 2),
+(1, 2, '귀검사(여)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt02.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_02.png', 3),
+(1, 1, '귀검사(남)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt01.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_01.png', 4);
 
 DELETE FROM ref_character_banner_detail WHERE version = 1 AND character_type = 4;
-INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES (1, 4, 1, '넨마스터(여)', '넨의 힘을 활용하기 위한 수련을 한 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg14_3.jpg', '/pg/characters/cim14');
-INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES (1, 4, 2, '스트라이커(여)', '육체를 극한까지 단련한 정통파 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg15_3.jpg', '/pg/characters/cim15');
-INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES (1, 4, 3, '스트리트파이터(여)', '이기는 싸움을 추구하는 실전 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg16_3.jpg', '/pg/characters/cim16');
-INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES (1, 4, 4, '그래플러(여)', '잡기 기술을 극한까지 연마한 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg17_3.jpg', '/pg/characters/cim17');
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(1, 4, 1, '넨마스터(여)', '넨의 힘을 활용하기 위한 수련을 한 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg14_3.jpg', '/pg/characters/cim14'),
+(1, 4, 2, '스트라이커(여)', '육체를 극한까지 단련한 정통파 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg15_3.jpg', '/pg/characters/cim15'),
+(1, 4, 3, '스트리트파이터(여)', '이기는 싸움을 추구하는 실전 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg16_3.jpg', '/pg/characters/cim16'),
+(1, 4, 4, '그래플러(여)', '잡기 기술을 극한까지 연마한 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg17_3.jpg', '/pg/characters/cim17');
+
+DELETE FROM ref_character_banner_detail WHERE version = 1 AND character_type = 3;
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(1, 3, 1, '넨마스터(남)', '넨의 힘을 육체에 접목한 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg10_3.jpg', '/pg/characters/cim10'),
+(1, 3, 2, '스트라이커(남)', '육체를 극한까지 단련한 정통파 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg11_3.jpg', '/pg/characters/cim11'),
+(1, 3, 3, '스트리트파이터(남)', '이기는 싸움을 추구하는 실전 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg12_3.jpg', '/pg/characters/cim12'),
+(1, 3, 4, '그래플러(남)', '타격에 잡기 기술을 접목한 격투가', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg13_3.jpg', '/pg/characters/cim13');
+
+DELETE FROM ref_character_banner_detail WHERE version = 1 AND character_type = 2;
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(1, 2, 1, '소드마스터', '마법을 검술에 접목시켜 사용하는 마검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg06_3.jpg', '/pg/characters/cim06'),
+(1, 2, 2, '데몬슬레이어', '마인의 힘을 활용할 수 있게 된 마검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg07_3.jpg', '/pg/characters/cim07'),
+(1, 2, 3, '베가본드', '마수에 깃든 마인의 힘을 억누르며 내공을 수련하는 마검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg08_3.jpg', '/pg/characters/cim08'),
+(1, 2, 4, '다크템플러', '죽은 자의 신 우시르를 섬기는 마검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg09_3.jpg', '/pg/characters/cim09'),
+(1, 2, 5, '블레이드', '비전의 납도술을 구사하는 마검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg62_3.jpg', '/pg/characters/cim62');
 
 DELETE FROM ref_character_banner_detail WHERE version = 1 AND character_type = 1;
-INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES (1, 1, 1, '웨펀마스터', '귀수에 깃든 귀신을 억누르며 검술 연마에 매진하는 귀검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg01_3.jpg', '/pg/characters/cim01');
-INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES (1, 1, 2, '소울브링어', '귀신과 소통함으로써 그 힘을 활용할 수 있게 된 귀검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg02_3.jpg', '/pg/characters/cim02');
-INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES (1, 1, 3, '버서커', '강력한 힘을 위해 부작용을 감수하며 카잔증후군을 받아들인 귀검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg03_3.jpg', '/pg/characters/cim03');
-INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES (1, 1, 4, '아수라', '파동의 힘을 느끼기 위해 스스로 시력을 포기한 귀검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg04_3.jpg', '/pg/characters/cim04');
-INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES (1, 1, 5, '검귀', '원귀의 혼과 융합하여 귀신과 인간의 경계에 선 귀검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg05_3.jpg', '/pg/characters/cim05');
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(1, 1, 1, '웨펀마스터', '귀수에 깃든 귀신을 억누르며 검술 연마에 매진하는 귀검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg01_3.jpg', '/pg/characters/cim01'),
+(1, 1, 2, '소울브링어', '귀신과 소통함으로써 그 힘을 활용할 수 있게 된 귀검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg02_3.jpg', '/pg/characters/cim02'),
+(1, 1, 3, '버서커', '강력한 힘을 위해 부작용을 감수하며 카잔증후군을 받아들인 귀검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg03_3.jpg', '/pg/characters/cim03'),
+(1, 1, 4, '아수라', '파동의 힘을 느끼기 위해 스스로 시력을 포기한 귀검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg04_3.jpg', '/pg/characters/cim04'),
+(1, 1, 5, '검귀', '원귀의 혼과 융합하여 귀신과 인간의 경계에 선 귀검사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg05_3.jpg', '/pg/characters/cim05');
+
+-- 홈 > 캐릭터 배너 (버전 2)
+DELETE FROM ref_character_banner WHERE version = 2;
+INSERT INTO ref_character_banner (version, character_type, character_desc, name_img_url, thumb_img_url, banner_order) VALUES
+(2, 17, '아처', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt17.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_16.png', 1),
+(2, 6, '거너(여)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt06.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_06.png', 2),
+(2, 5, '거너(남)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt05.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_05.png', 3),
+(2, 8, '마법사(여)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt08.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_08.png', 4),
+(2, 7, '마법사(남)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt07.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_07.png', 5);
+
+DELETE FROM ref_character_banner_detail WHERE version = 2 AND character_type = 17;
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(2, 17, 1, '뮤즈', '선현궁 연주로 아군의 사기를 북돋우는 궁수', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg64_3.jpg', '/pg/characters/cim64'),
+(2, 17, 2, '트래블러', '자유 여행자 조합의 신비한 도구를 사용하는 궁수', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg65_3.jpg', '/pg/characters/cim65'),
+(2, 17, 3, '헌터', '크로스슈터로 요수와 적들을 사냥하는 궁수', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg66_3.jpg', '/pg/characters/cim66'),
+(2, 17, 4, '비질란테', '요기를 받아들여 요수화가 가능해진 궁수', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg67_3.jpg', '/pg/characters/cim67'),
+(2, 17, 5, '키메라', '투척 무기와 유물을 활용해 적들을 유린하는 괴짜', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg68_3.jpg', '/pg/characters/cim68');
+
+DELETE FROM ref_character_banner_detail WHERE version = 2 AND character_type = 6;
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(2, 6, 1, '레인저(여)', '리볼버 사격과 리볼버 하단에 장착된 건블레이드를 활용한 체술이 특기인 거너', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg22_3.jpg', '/pg/characters/cim22'),
+(2, 6, 2, '런처(여)', '중화기를 이용한 원거리 공격이 특기인 거너', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg23_3.jpg', '/pg/characters/cim23'),
+(2, 6, 3, '메카닉(여)', '로봇과 기계장치를 활용한 공격이 특기인 거너', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg24_3.jpg', '/pg/characters/cim24'),
+(2, 6, 4, '스핏파이어(여)', '다양한 화기를 활용한 공격이 특기인 거너', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg25_3.jpg', '/pg/characters/cim25'),
+(2, 6, 5, '패러메딕', '화력 지원 및 피해 방지가 특기인 거너', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg69_3.jpg', '/pg/characters/cim69');
+
+DELETE FROM ref_character_banner_detail WHERE version = 2 AND character_type = 5;
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(2, 5, 1, '레인저(남)', '리볼버를 이용한 빠른 사격과 체술이 특기인 거너', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg18_3.jpg', '/pg/characters/cim18'),
+(2, 5, 2, '런처(남)', '중화기를 이용한 원거리 공격이 특기인 거너', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg19_3.jpg', '/pg/characters/cim19'),
+(2, 5, 3, '메카닉(남)', '로봇과 기계장치를 활용한 공격이 특기인 거너', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg20_3.jpg', '/pg/characters/cim20'),
+(2, 5, 4, '스핏파이어(남)', '다양한 화기를 활용한 공격이 특기인 거너', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg21_3.jpg', '/pg/characters/cim21'),
+(2, 5, 5, '어썰트(남)', '개조된 신체와 무기를 활용한 공격이 특기인 거너', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg63_3.jpg', '/pg/characters/cim63');
+
+DELETE FROM ref_character_banner_detail WHERE version = 2 AND character_type = 8;
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(2, 8, 1, '엘레멘탈 마스터', '4가지 속성의 원소 마법을 고르게 연구한 마법사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg31_3.jpg', '/pg/characters/cim31'),
+(2, 8, 2, '소환사', '소환 마법을 극한까지 연구한 마법사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg32_3.jpg', '/pg/characters/cim32'),
+(2, 8, 3, '배틀메이지', '원소의 힘을 응축한 체이서를 이용하는 근접전 마법사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg33_3.jpg', '/pg/characters/cim33'),
+(2, 8, 4, '마도학자', '원소의 힘을 다양한 형태로 변환하는 마도학에 집중한 마법사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg34_3.jpg', '/pg/characters/cim34'),
+(2, 8, 5, '인챈트리스', '저주와 인형술을 사용하는 변덕쟁이 마녀', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg35_3.jpg', '/pg/characters/cim35');
+
+DELETE FROM ref_character_banner_detail WHERE version = 2 AND character_type = 7;
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(2, 7, 1, '엘레멘탈 바머', '어비스의 힘을 빌려 원소 마법의 힘을 극대화 시킨 마법사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg26_3.jpg', '/pg/characters/cim26'),
+(2, 7, 2, '빙결사', '수속성 마법을 극한까지 연구한 마법사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg27_3.jpg', '/pg/characters/cim27'),
+(2, 7, 3, '블러드 메이지', '혈기 그 자체를 힘의 근원으로 삼은 마법사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg28_3.jpg', '/pg/characters/cim28'),
+(2, 7, 4, '스위프트 마스터', '바람의 힘을 자유자재로 다루는 마법사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg29_3.jpg', '/pg/characters/cim29'),
+(2, 7, 5, '디멘션워커', '차원 저편의 비밀을 연구하는 마법사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg30_3.jpg', '/pg/characters/cim30');
+
+-- 홈 > 캐릭터 배너 (버전 3)
+DELETE FROM ref_character_banner WHERE version = 3;
+INSERT INTO ref_character_banner (version, character_type, character_desc, name_img_url, thumb_img_url, banner_order) VALUES
+(3, 10, '크루세이더(여)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt10.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_10.png', 1),
+(3, 9, '크루세이더(남)', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt09.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_09.png', 2),
+(3, 13, '마창사', 'https://bbscdn.df.nexon.com/pg/characters/img/main/mc_txt13.png', 'https://bbscdn.df.nexon.com/pg/characters/img/thum/thum_char_13.png', 3);
+
+DELETE FROM ref_character_banner_detail WHERE version = 3 AND character_type = 13;
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(3, 13, 1, '뱅가드', '늘 전투의 최전방에서 다수의 적과 싸우며 파괴하는 마창사', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg52_3.jpg', '/pg/characters/cim52'),
+(3, 13, 2, '듀얼리스트', '마창의 힘을 대인 전투에 특화시킨 창술의 달인', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg53_3.jpg', '/pg/characters/cim53'),
+(3, 13, 3, '드래고니안 랜서', '서번트 랜스를 활용하여 마수를 사냥하는 마수 사냥꾼', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg54_3.jpg', '/pg/characters/cim54'),
+(3, 13, 4, '리퍼', '어둠의 힘을 다루는 강력한 암살자', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg55_3.jpg', '/pg/characters/cim55');
+
+DELETE FROM ref_character_banner_detail WHERE version = 3 AND character_type = 10;
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(3, 10, 1, '크루세이더(여)', '신성한 빛의 힘으로 적을 섬멸하고 아군을 지원하는 프리스트', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg40_3.jpg', '/pg/characters/cim40'),
+(3, 10, 2, '이단심판관', '교단의 율법에 따라 적을 심판하는 잔혹한 성직자', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg41_3.jpg', '/pg/characters/cim41'),
+(3, 10, 3, '무녀', '염주와 부적, 그리고 신룡의 힘으로 마물을 퇴치하는 프리스트', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg42_3.jpg', '/pg/characters/cim42'),
+(3, 10, 4, '미스트리스', '원죄의 힘으로 악을 유혹하여 처단하는 프리스트', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg43_3.jpg', '/pg/characters/cim43');
+
+DELETE FROM ref_character_banner_detail WHERE version = 3 AND character_type = 9;
+INSERT INTO ref_character_banner_detail (version, character_type, character_detail_type, character_name, character_intro, img_url, homepage_link_url) VALUES
+(3, 9, 1, '크루세이더(남)', '신의 가호를 받아 강력한 신성력을 가지게 된 프리스트', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg36_3.jpg', '/pg/characters/cim36'),
+(3, 9, 2, '인파이터', '악을 섬멸하기 위해 권격을 극한까지 수련한 프리스트', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg37_3.jpg', '/pg/characters/cim37'),
+(3, 9, 3, '퇴마사', '주술과 거병을 활용하여 마물을 퇴치하는 프리스트', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg38_3.jpg', '/pg/characters/cim38'),
+(3, 9, 4, '어벤저', '악을 섬멸하기 위해 악마의 힘을 받아들인 프리스트', 'https://bbscdn.df.nexon.com/pg/characters/img/bg/bg39_3.jpg', '/pg/characters/cim39');
 
 
 -----------[ 세팅 환경에 맞게 추가해야 하는 부분 ]--------------
